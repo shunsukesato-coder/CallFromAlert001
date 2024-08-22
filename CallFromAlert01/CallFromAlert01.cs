686dfe0237b6f58b7b2f25e162c3d831e71daadd
Shunsuke Sato <shunsukesato@microsoft.com>
2024-08-22 16:48:13 +0900

    commit 8/22

--- CallFromAlert01/CallFromAlert01.cs
+++ CallFromAlert01/CallFromAlert01.cs
@@ -26,7 +26,23 @@ namespace CallFromAlert01.Functions
             // Request body を JSON として処理
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            // 必要に応じて、data に対する操作をここで実行
+            // JSON データからフィールドを抽出してログに出力
+            var severity = data?.data?.essentials?.severity;
+            var signalType = data?.data?.essentials?.signalType;
+            var monitoringService = data?.data?.essentials?.monitoringService;
+            var firedDateTime = data?.data?.essentials?.firedDateTime;
+            var description = data?.data?.essentials?.description;
+            var alertId = data?.data?.essentials?.alertId;
+            var alertCategory = data?.data?.alertContext?.AlertCategory;
+
+            log.LogInformation("Alert Details:\n" +
+                               $"重大度: {severity}\n" +
+                               $"Signal Type: {signalType}\n" +
+                               $"Monitoring Service: {monitoringService}\n" +
+                               $"発生日時: {firedDateTime}\n" +
+                               $"説明: {description}\n" +
+                               $"アラート ID: {alertId}\n" +
+                               $"アラートカテゴリー: {alertCategory}");
 
             return new OkObjectResult("Alert received successfully");
         }
