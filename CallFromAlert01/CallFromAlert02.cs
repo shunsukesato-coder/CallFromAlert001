d32d287befc152527c9bae89c75f9bb0be863ed6
Shunsuke Sato <shunsukesato@microsoft.com>
2025-01-02 21:27:21 +0900

    commit

---
+++ CallFromAlert01/CallFromAlert02.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallFromAlert01
+{
+    public static class AlertFunction
+    {
+        [FunctionName("ReceiveAlertFunction02")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "alert")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            using var reader = new StreamReader(req.Body);
+            var requestBody = await reader.ReadToEndAsync();
+
+            log.LogInformation("Received request body: {RequestBody}", requestBody);
+
+            // Request body を AlertData オブジェクトとしてデシリアライズ
+            AlertData data = JsonConvert.DeserializeObject<AlertData>(requestBody);
+
+            if (data != null)
+            {
+                // JSON データからフィールドを抽出してログに出力
+                log.LogInformation("Alert Details:\n" +
+                                   $"重大度: {data.data?.essentials?.severity}\n" +
+                                   $"Signal Type: {data.data?.essentials?.signalType}\n" +
+                                   $"Monitoring Service: {data.data?.essentials?.monitoringService}\n" +
+                                   $"発生日時: {data.data?.essentials?.firedDateTime}\n" +
+                                   $"説明: {data.data?.essentials?.description}\n" +
+                                   $"アラート ID: {data.data?.essentials?.alertId}\n" +
+                                   $"アラートカテゴリー: {data.data?.alertContext?.AlertCategory}");
+            }
+            else
+            {
+                log.LogWarning("Received request body could not be parsed.");
+            }
+
+            return new OkObjectResult("Alert received successfully");
+        }
+    }
+}
